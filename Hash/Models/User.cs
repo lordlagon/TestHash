933273a9d5c7d4b17d872b916b27b3305d79da31
Andre Xavier <andre@azuris.com.br>
2018-11-06 12:41:54 -0200

    Comit inicial

---
+++ Hash/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hash.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Passwordhash { get; set; }
+        public string Email { get; set; }
+    }
+}
